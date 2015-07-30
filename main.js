4f1803eed0d90809682a1d6beed41bfad8e4c5cb
Keisuke KITA <keisuke.kita@linecorp.com>
2015-07-30 09:53:52 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+require('crash-reporter').start();
+
+var mainWindow = null;
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('ready', function () {
+  var atomScreen = require('screen');
+  var size = atomScreen.getPrimaryDisplay().workAreaSize;
+
+  mainWindow = new BrowserWindow({
+    width: 300,
+    height: 400,
+    resizable: false,
+    transparent: false,
+    frame: false
+  });
+
+  mainWindow.setAlwaysOnTop(true);
+  mainWindow.setSkipTaskbar(true);
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+});
